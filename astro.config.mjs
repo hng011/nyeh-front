c36a13ffea7b49bb3576aca7e761efd31869064a
hng011 <93465725+hng011@users.noreply.github.com>
2026-08-01 00:06:14 +0700

    Change site URL to 'https://kaktus.foo'
    Updated site URL and base path in the configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vue from '@astrojs/vue';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://hng011.github.io',
-  base: '/nyeh-front',
+  site: 'https://kaktus.foo',
+
   integrations: [vue()],
 
   vite: {
